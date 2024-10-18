45448684913632a27389386e0ded16757e3b26a6
Lautaro Di Sanza <lautarod@gmail.com>
2024-10-18 12:23:00 -0300

    *

--- backend/src/trade/trade.module.ts
+++ backend/src/trade/trade.module.ts
@@ -11,15 +11,3 @@ import { TradeSchema } from './schemas/trade.schema';
   providers: [TradeService],
 })
 export class TradeModule {}
-
-// import { Module } from '@nestjs/common';
-// import { NftController } from './nft.controller';
-// import { NftService } from './nft.service';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { NFTSchema } from './schemas/nft.schema';
-// @Module({
-//   imports: [MongooseModule.forFeature([{ name: 'NFT', schema: NFTSchema }])],
-//   controllers: [NftController],
-//   providers: [NftService],
-// })
-// export class NftModule {}
